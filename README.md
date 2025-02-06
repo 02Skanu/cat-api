7bb35e4b95555609f171a87f60c67dfcd6321f7c
02Skanu <babyrhksdn@naver.com>
2025-02-06 16:43:41 +0900

    add fox

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 
 # 나만 없어 고양이
 
-![Cat Image](https://cdn2.thecatapi.com/images/MTkwMzAzNw.jpg)
+![Cat Image](https://cdn2.thecatapi.com/images/33v.gif)
 
-⏳ 업데이트 시간: 2025-02-06 07:33:39 (KRW)
+# 여우도 없어
+![Fox Image](https://randomfox.ca/images/44.jpg)
+⏳ 업데이트 시간: 2025-02-06 16:43:28 (KRW)
 
 ---
--- cat.py
+++ cat.py
@@ -7,6 +7,7 @@ load_dotenv()
 # TheCatAPI 키 설정
 API_KEY = os.getenv("CAT_API_KEY")
 URL = f"https://api.thecatapi.com/v1/images/search?api_key={API_KEY}"
+FOX_URL = "https://randomfox.ca/floof/"
 
 # README 파일 경로
 README_PATH = "README.md"
@@ -18,30 +19,33 @@ def get_cat_info():
         if data:
             cat_info = data[0]
             image_url = cat_info.get("url")
-            breed_info = cat_info.get("breeds", [{}])[0]
-            breed_name = breed_info.get("name", "Unknown")
-            description = breed_info.get("description", "No description available.")
-            wikipedia_url = breed_info.get("wikipedia_url", "No Wikipedia link available.")
 
             return {
                 "image_url": image_url,
-                "breed_name": breed_name,
-                "description": description,
-                "wikipedia_url": wikipedia_url
+
             }
     return None
 
+def get_fox_info():
+    response = requests.get(FOX_URL)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("image", "No image available")
+    return None
 
 def update_readme():
     cat_info = get_cat_info()
+    fox_image_url = get_fox_info()
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    if cat_info:
+    if cat_info and fox_image_url:
         readme_content = f"""
 # 나만 없어 고양이
 
 ![Cat Image]({cat_info['image_url']})
 
+# 여우도 없어
+![Fox Image]({fox_image_url})
 ⏳ 업데이트 시간: {now} (KRW)
 
 ---
